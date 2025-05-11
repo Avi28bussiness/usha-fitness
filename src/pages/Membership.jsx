19afe2feff8617e51fefe2feb7d28373e95028db
Avi28bussiness <thakur28aviral@gmail.com>
2025-05-12 01:20:04 +0530

    right fix

--- src/pages/Membership.jsx
+++ src/pages/Membership.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Star } from 'lucide-react';
-import { right } from '@popperjs/core';
+
 
 export default function Membership() {
   const [plans, setPlans] = useState([]);
